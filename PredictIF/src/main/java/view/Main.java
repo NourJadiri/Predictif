77735c80baec00e93de77f841f07d457b08ad4b3
NourJadiri <100280446+NourJadiri@users.noreply.github.com>
2023-04-25 11:38:46 +0200

    Ajout test ajout Medium et Employe
    J'ai pas fait expres de virer l'authentification client oups

--- PredictIF/src/main/java/view/Main.java
+++ PredictIF/src/main/java/view/Main.java
@@ -10,10 +10,9 @@ import dao.JpaUtil;
 import metier.modele.*;
 import metier.service.Service;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -36,9 +35,9 @@ public class Main {
     
     public static void testerInscriptionClient(){
        Client ada = new Client("Lovelace", "Ada", new java.util.Date(System.currentTimeMillis()),"130 Avenue Albert Einstein","ada.lovelace@insa-lyon.fr","0669696969", "LAda");
-       Service Sc = new Service();
+       Service sc = new Service();
         for (int i = 0; i < 2; i++) {
-            Long id = Sc.inscriptionClient(ada);
+            Long id = sc.inscriptionClient(ada);
             if(id != null){
                 System.out.println("> Succès inscription");
             } else {
@@ -75,12 +74,13 @@ public class Main {
         Spirite spirite = new Spirite("The Ghost Whisperer", "Female", "I can communicate with the dead", "Crystal ball");
 
         // Creating a new Cartomancier object
-        Cartomancier cartomancier = new Cartomancier("Madame Destiny", "Female", "I can predict your future using tarot cards");
+        Cartomancien cartomancier = new Cartomancien("Madame Destiny", "Female", "I can predict your future using tarot cards");
 
         // Creating a new Astrologue object
         Astrologue astrologue = new Astrologue("Master of Astrology", "2020", "Professor Stars", "Male", "I can read the stars and predict your fate");
 
         Service sc = new Service();
+
         sc.ajouterMedium(spirite);
         sc.ajouterMedium(cartomancier);
         sc.ajouterMedium(astrologue);
@@ -89,19 +89,54 @@ public class Main {
     public static void testerAjoutConsultation() {
         Service sc = new Service();
 
-        Employe Camille = sc.rechercherEmploye(1L);
+        // Create a list to hold the consultations
+        List<Consultation> consultations = new ArrayList<>();
 
-        Medium gwen = new Spirite("Gwen","F","Bonsoir","Livre");
-        sc.ajouterMedium(gwen);
+        // Create the first consultation
+        Date date1 = new Date();
+        Date heure1 = new Date();
+        Employe employe1 = new Employe("John", "Doe", 'M', "1234567890", "john.doe@example.com", "password");
+        Client client1 = new Client("Jane", "Doe", new Date(), "123 Main St", "jane.doe@example.com", "1234567890", "password");
+        Medium medium1 = new Cartomancien("Gwen", "Female", "A tarot reading involves a deck of cards and a reader who interprets them to provide insights and guidance.");
+        String commentaire1 = "This was a great consultation.";
+        Consultation consultation1 = new Consultation(date1, heure1, employe1, client1, medium1, commentaire1);
+        consultations.add(consultation1);
 
-        Client ada = sc.rechercherClientparId(1L);
+        // Create the second consultation
+        Date date2 = new Date();
+        Date heure2 = new Date();
+        Employe employe2 = new Employe("Alice", "Smith", 'F', "0987654321", "alice.smith@example.com", "password");
+        Client client2 = new Client("Bob", "Johnson", new Date(), "456 Main St", "bob.johnson@example.com", "0987654321", "password");
+        Medium medium2 = new Astrologue("Twisted Fate", "Male","Cards are cool, Stars are the future","Divination","2020");
+        String commentaire2 = "I found the astrology reading to be very insightful.";
+        Consultation consultation2 = new Consultation(date2, heure2, employe2, client2, medium2, commentaire2);
+        consultations.add(consultation2);
 
-        Consultation c1 = new Consultation(new Date(), new Date(), Camille, ada, gwen, "oui" );
-        sc.ajouterConsultation(c1);
+        // Create the third consultation
+        Date date3 = new Date();
+        Date heure3 = new Date();
+        Employe employe3 = new Employe("David", "Lee", 'M', "5555555555", "david.lee@example.com", "password");
+        Client client3 = new Client("Susan", "Kim", new Date(), "789 Main St", "susan.kim@example.com", "5555555555", "password");
+        Medium medium3 = new Spirite("Irma","Female","bonsoir","Book");
+        String commentaire3 = "The spiritual counseling helped me find inner peace.";
+        Consultation consultation3 = new Consultation(date3, heure3, employe3, client3, medium3, commentaire3);
+        consultations.add(consultation3);
 
-        System.out.println(c1);
+        // Create the fourth consultation
+        Date date4 = new Date();
+        Date heure4 = new Date();
+        Employe employe4 = new Employe("Emily", "Davis", 'F', "1111111111", "emily.davis@example.com", "password");
+        Client client4 = new Client("Mike", "Jackson", new Date(), "101 Main St", "mike.jackson@example.com", "1111111111", "password");
+        Medium medium4 = new Cartomancien("Cartomancy Reading", "Divination", "A cartomancy reading involves using a deck of cards, such as tarot or oracle cards, to provide insights and guidance.");
+        String commentaire4 = "The cartomancy reading was spot on.";
+        Consultation consultation4 = new Consultation(date4, heure4, employe4, client4, medium4, commentaire4);
+        consultations.add(consultation4);
+
+        // Persisting all consultation objects
+        for(Consultation consultation : consultations){
+            sc.ajouterConsultation(consultation);
+        }
+
     }
-
-    
-    
+ 
 }
