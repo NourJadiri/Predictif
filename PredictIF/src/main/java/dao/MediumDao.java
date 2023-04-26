421ca902fb451349858c9ae20488b2ea75e47062
Nour <mohamed-nour.eljadiri@insa-lyon.fr>
2023-04-26 17:20:08 +0200

    filtrage de medium fonctionnel

--- PredictIF/src/main/java/dao/MediumDao.java
+++ PredictIF/src/main/java/dao/MediumDao.java
@@ -3,6 +3,9 @@ package dao;
 import metier.modele.Medium;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MediumDao {
     public void create(Medium medium){
@@ -13,5 +16,43 @@ public class MediumDao {
     public Medium findById(Long mediumId){
         EntityManager em = JpaUtil.obtenirContextePersistance();
         return em.find(Medium.class , mediumId);
+    }
+
+    public List<Medium> filter(String genre , ArrayList<String> types){
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+
+        List<Medium> resultat;
+
+        String queryBase = "SELECT m FROM Medium m WHERE ";
+
+        String mediumGenderFilter = "";
+        String mediumTypeFilter = "";
+
+        // filtre par genre de médium
+        // Si le genre n'est pas "all" on rentre dans le filtre
+        if( genre != "all"){
+            mediumGenderFilter = mediumGenderFilter + "m.genre = \"" + genre + "\" ";
+            if(!types.isEmpty()){
+                mediumGenderFilter += "AND";
+            }
+        }
+
+        // filtre par type de médium
+        if(!types.isEmpty()){
+            for (int i = 0 ; i < types.size() ; i++) {
+                mediumTypeFilter = mediumTypeFilter + " m.type_medium = \"" + types.get(i) + "\" ";
+                if(i < types.size() - 1){
+                    mediumTypeFilter += "OR ";
+                }
+            }
+        }
+
+        String queryString = queryBase + mediumGenderFilter + mediumTypeFilter;
+
+        TypedQuery<Medium> query = em.createQuery(queryString , Medium.class);
+
+        resultat = query.getResultList();
+
+        return resultat;
     }
 }
